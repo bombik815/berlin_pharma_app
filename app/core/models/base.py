ae3017f233fe130334726d8447887158ae17e1d1
bombik815 <bombik815@gmail.com>
2024-10-02 16:58:27 +0300

    create declarative base

---
+++ app/core/models/base.py
@@ -0,0 +1,17 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import declared_attr
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from core.config import settings
+from utils import camel_case_to_snake_case
+
+# Базовая модель для всех моделей
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
