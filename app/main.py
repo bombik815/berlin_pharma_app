34f76fd26901c51b80366335900fb6e53e468f1e
bombik815 <bombik815@gmail.com>
2024-10-09 15:33:29 +0300

    use ORJSON

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 import uvicorn
 
 from core.config import settings
@@ -12,21 +13,25 @@ from core.models import db_helper
 @asynccontextmanager
 async def lifespan(app):
     # startup
-    print('Starting up database...')
+    print("Starting up database...")
     yield
     # shutdown
-    print('Shutting down database...')
+    print("Shutting down database...")
     await db_helper.dispose()
 
 
-main_app = FastAPI(lifespan=lifespan)
+main_app = FastAPI(
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+)
 
 main_app.include_router(api_router, prefix=settings.api.prefix)
 
 
-
-if __name__ == '__main__':
-    uvicorn.run('main:main_app',
-                host=settings.run.host,
-                port=settings.run.port,
-                reload=True)
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:main_app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
