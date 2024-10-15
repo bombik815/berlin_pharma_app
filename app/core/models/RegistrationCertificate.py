8f13d328f960a4564d9ee229ba5bc0fc2e69dd4f
bombik815 <bombik815@gmail.com>
2024-10-15 14:33:55 +0300

    change type of field creatAt_Reg_Ce -> DateTime

--- app/core/models/RegistrationCertificate.py
+++ app/core/models/RegistrationCertificate.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import String
+from sqlalchemy import String, DateTime
 from sqlalchemy.orm import mapped_column, Mapped
 
 from .base import Base
@@ -8,7 +8,6 @@ from .base import Base
 
 class RegistrationCertificate(Base):
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
     trade_Name: Mapped[str] = mapped_column(String, nullable=False)
     reg_Cert_Number: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    createAt_Reg_Cer: Mapped[datetime] = mapped_column(String, nullable=False)
+    createAt_Reg_Cer: Mapped[datetime] = mapped_column(DateTime, nullable=False)
