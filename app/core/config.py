1adfaa45256f75a3daeab6f749457517fed3300b
bombik815 <bombik815@gmail.com>
2024-10-16 14:18:43 +0300

    add version api, change host adress

--- app/core/config.py
+++ app/core/config.py
@@ -2,20 +2,27 @@ from pydantic import BaseModel
 from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-''' Класс настройки запуска app'''
+""" Класс настройки запуска app"""
+
+
 class RunConfig(BaseModel):
-    host: str = '127.0.0.1' # адрес сервера
-    port: int = 8000        # порт сервера
+    host: str = "0.0.0.0"  # "127.0.0.1"  # адрес сервера
+    port: int = 8000  # порт сервера
+
 
 class ApiV1Prefix(BaseModel):
-    prefix: str = '/v1' # префикс запросов для версии API v1
+    prefix: str = "/v1"  # префикс запросов для версии API v1
+    registration_certificates: str = "/registration_certificates"
+
 
 class ApiPrefix(BaseModel):
-    prefix: str = '/api' # префикс запросов
-    v1: ApiV1Prefix = ApiV1Prefix() # префикс запросов для версии API v1
+    prefix: str = "/api"  # префикс запросов
+    v1: ApiV1Prefix = ApiV1Prefix()  # префикс запросов для версии API v1
 
 
-''' Настройки подключение к БД '''
+""" Настройки подключение к БД """
+
+
 class DatabaseConfig(BaseModel):
     url: PostgresDsn  # ссылка подключение к БД
     echo: bool = False
@@ -23,19 +30,21 @@ class DatabaseConfig(BaseModel):
     max_overflow: int = 50
     pool_size: int = 10
 
-''' Файл для всех настроеек приложения  '''
+
+""" Файл для всех настроеек приложения  """
+
+
 class Settings(BaseSettings):
     # настройки модели
     model_config = SettingsConfigDict(
-        env_file=".env", # путь к файлу с переменными окружения
-        case_sensitive=False, # регистр в именах моделей
-        env_nested_delimiter="__", # разделитель в именах моделей
-        env_prefix="APP_CONFIG__" # префикс для переменных окружения
+        env_file=".env",  # путь к файлу с переменными окружения
+        case_sensitive=False,  # регистр в именах моделей
+        env_nested_delimiter="__",  # разделитель в именах моделей
+        env_prefix="APP_CONFIG__",  # префикс для переменных окружения
     )
-    run: RunConfig = RunConfig() # вызов настройки запуска приложения
-    api: ApiPrefix = ApiPrefix() # префикс запросов
-    db: DatabaseConfig           # настройки подключения к БД
+    run: RunConfig = RunConfig()  # вызов настройки запуска приложения
+    api: ApiPrefix = ApiPrefix()  # префикс запросов
+    db: DatabaseConfig  # настройки подключения к БД
 
 
 settings = Settings()
-
