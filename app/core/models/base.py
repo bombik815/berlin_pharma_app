280e5764f0ca287515db25fb7f81f0848d529d8d
bombik815 <bombik815@gmail.com>
2024-10-17 16:34:13 +0300

    change type for ID -> UUID

--- app/core/models/base.py
+++ app/core/models/base.py
@@ -1,4 +1,7 @@
+import uuid
+
 from sqlalchemy import MetaData, Integer
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import declared_attr
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -10,7 +13,9 @@ from utils import camel_case_to_snake_case
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
