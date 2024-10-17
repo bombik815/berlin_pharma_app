69a5c322b07a4ee0d86f9f8928bde9400dc2b8cc
bombik815 <bombik815@gmail.com>
2024-10-17 16:36:16 +0300

    create routers for get_all, get_by_id and create for registration certificates

--- app/api/api_v1/registration_certificate/router.py
+++ app/api/api_v1/registration_certificate/router.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+from uuid import UUID
 
 from fastapi import (
     APIRouter,
@@ -8,38 +9,73 @@ from fastapi import (
 )
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.config import settings
+
 from core.models import db_helper
-from .crud import get_registration_certificates as get_all_certifications
-from .dependencies import registration_certificate_by_id
-from .schemas import SRegistrationCertificate
+
+from .dao import RegistrationCertificateDAO as certificateDAO
+
+from .schemas import (
+    SRegistrationCertificate,
+    SRegistrationCertificateUpdate,
+    SRegistrationCertificateCreate,
+)
 
 router = APIRouter(tags=["Регистрационные удостоверения"])
 
 
-@router.get(
-    "/",
-    response_model=list[SRegistrationCertificate],
-    summary="Получить все Регистрационные удостоверения",
-)
+@router.get("/", summary="Получить все Регистрационные удостоверения")
 async def get_registration_certificates(
-    session: Annotated[
-        AsyncSession,
-        Depends(db_helper.session_getter),
-    ],
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)]
 ):
-    certifications = await get_all_certifications(session=session)
-    return certifications
+    return await certificateDAO.get_all_registration_certificates(session=session)
+
+
+@router.post(
+    "/add/",
+    response_model=SRegistrationCertificate,
+    status_code=status.HTTP_201_CREATED,
+    summary="Создать Регистрационное удостоверение",
+)
+async def create_registration_certificate(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    certificate_in: SRegistrationCertificateCreate,
+):
+    return await certificateDAO.add_registration_certificate(
+        session=session,
+        certificate_in=certificate_in,
+    )
 
 
 @router.get(
-    "/{certificate_id}/",
-    response_model=SRegistrationCertificate,
-    summary="Получить Регистрационное удостоверение по ID",
+    "/{certificate_id}/", summary="Получить Регистрационное удостоверение по ID"
 )
-async def get_registration_certificate(
-    registration_certificate: SRegistrationCertificate = Depends(
-        registration_certificate_by_id
-    ),
+async def get_registration_certificate_by_id(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    certificate_id: UUID,
 ):
-    return registration_certificate
+    result = await certificateDAO.get_registration_certificate_by_id(
+        session=session, certificate_id=certificate_id
+    )
+    if result:
+        return SRegistrationCertificate.from_orm(result)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Регистрационное удостоверение не найдено",
+        )
+
+
+# @router.put("/{certificate_id}/", summary="Обновить Регистрационное удостоверение")
+# async def update_registration_certificate(
+#     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+#     certificate_update: SRegistrationCertificateUpdate,
+#     certificate: SRegistrationCertificate = Depends(
+#         certificateDAO.get_registration_certificate_by_id
+#     ),
+# ):
+#     result = await certificateDAO.update_registration_certificate(
+#         session=session,
+#         certificate=certificate,
+#         certificate_update=certificate_update,
+#     )
+#     return result
