95b66aa73e76f1c2f59a65a2fab92ec5fd04f3b3
bombik815 <bombik815@gmail.com>
2024-10-17 18:35:40 +0300

    added alias for creatAt_Reg_Cer

--- app/api/api_v1/registration_certificate/schemas.py
+++ app/api/api_v1/registration_certificate/schemas.py
@@ -1,18 +1,21 @@
 from datetime import datetime
+from uuid import UUID
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class SRegistrationCertificateBase(BaseModel):
     trade_Name: str
     reg_Cert_Number: str
-    createAt_Reg_Cer: datetime
+    createAt_Reg_Cer: datetime = Field(
+        ..., alias="createAt_Reg_Cer"
+    )  # Используем 'alias' только если необходимо
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class SRegistrationCertificateCreate(SRegistrationCertificateBase):
-    pass
+    model_config = ConfigDict(from_attributes=True)
 
 
 class SRegistrationCertificateUpdate(SRegistrationCertificateBase):
@@ -20,9 +23,15 @@ class SRegistrationCertificateUpdate(SRegistrationCertificateBase):
 
 
 class SRegistrationCertificatePartial(SRegistrationCertificateBase):
-    pass
+
+    trade_Name: str | None = None
+    reg_Cert_Number: str | None = None
+    createAt_Reg_Cer: datetime | None = None
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class SRegistrationCertificate(SRegistrationCertificateBase):
     model_config = ConfigDict(from_attributes=True)
-    id: int
+
+    id: UUID
