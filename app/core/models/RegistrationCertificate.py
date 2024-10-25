e37581d3653033ae8f4739ec6f95043cc62cdf5f
bombik815 <bombik815@gmail.com>
2024-10-25 14:36:30 +0300

    add is_Active field

--- app/core/models/RegistrationCertificate.py
+++ app/core/models/RegistrationCertificate.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import String, DateTime
+from sqlalchemy import String, DateTime, Boolean
 from sqlalchemy.orm import mapped_column, Mapped
 
 from .base import Base
@@ -11,3 +11,7 @@ class RegistrationCertificate(Base):
     trade_Name: Mapped[str] = mapped_column(String, nullable=False)
     reg_Cert_Number: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     createAt_Reg_Cer: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    is_Active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    def __repr__(self) -> str:
+        return f'<RegistrationCertificate(trade_Name="{self.trade_Name}", reg_Cert_Number="{self.reg_Cert_Number}")>'
