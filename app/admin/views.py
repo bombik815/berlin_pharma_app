93770a4adb31c3a4346cea4343e97600786b4962
bombik815 <bombik815@gmail.com>
2024-10-25 14:37:45 +0300

    refactoring tools

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,41 +1,63 @@
+from datetime import datetime
+
 from sqladmin import ModelView
 
 from app.core.models import RegistrationCertificate
 
+"""DOC-HELPERS https://aminalaee.dev/sqladmin/configurations/ """
+"""
+Defines the configuration settings for the RegistrationCertificate model view in the admin panel.
 
+    Columns displayed: id, trade_Name, reg_Cert_Number, createAt_Reg_Cer
+    Columns excluded from details view: id
+    Columns searchable by default: trade_Name, reg_Cert_Number
+    Columns sortable by default: trade_Name
+
+    Settings:
+        - Deletion allowed: Yes
+        - Singular name: "Регистрационный сертификат"
+        - Plural name: "Регистрационные сертификаты"
+        - Icon: "fa-solid fa-certificate"
+"""
+
+
+# noinspection PyTypeChecker
 class RegistrationCertificateAdmin(ModelView, model=RegistrationCertificate):
+    group_name = "Мастер данных"
+
+    column_labels = {
+        RegistrationCertificate.trade_Name: "Наименование",
+        RegistrationCertificate.reg_Cert_Number: "Номер РУ",
+        RegistrationCertificate.createAt_Reg_Cer: "Дата регистрации",
+    }
+
+    save_as = True
+
     column_list = [
-        RegistrationCertificate.id,
+        # RegistrationCertificate.id,
         RegistrationCertificate.trade_Name,
         RegistrationCertificate.reg_Cert_Number,
         RegistrationCertificate.createAt_Reg_Cer,
+        RegistrationCertificate.is_Active,
     ]
-    # column_details_exclude_list = [Users.hashed_password]
+    # column_details_exclude_list = [RegistrationCertificate.id]
+    column_searchable_list = [
+        RegistrationCertificate.trade_Name,
+        RegistrationCertificate.reg_Cert_Number,
+        RegistrationCertificate.is_Active,
+    ]
+    column_sortable_list = [
+        RegistrationCertificate.trade_Name,
+    ]
+    column_default_sort = [
+        (RegistrationCertificate.trade_Name, True),
+    ]
+    # column_formatters = {RegistrationCertificate.trade_Name: lambda m, a: m.trade_Name[:10]}
+
     can_delete = True
     name = "Регистрационный сертификат"
     name_plural = "Регистрационные сертификаты"
     icon = "fa-solid fa-certificate"
 
-
-# class HotelsAdmin(ModelView, model=Hotels):
-#     column_list = [c.name for c in Hotels.__table__.c] + [Hotels.rooms]
-#     name = "Отель"
-#     name_plural = "Отели"
-#     icon = "fa-solid fa-hotel"
-#
-#
-# class RoomsAdmin(ModelView, model=Rooms):
-#     column_list = [c.name for c in Rooms.__table__.c] + [Rooms.hotel, Rooms.bookings]
-#     name = "Номер"
-#     name_plural = "Номера"
-#     icon = "fa-solid fa-bed"
-#
-#
-# class BookingsAdmin(ModelView, model=Bookings):
-#     column_list = [c.name for c in Bookings.__table__.c] + [
-#         Bookings.user,
-#         # Bookings.room,
-#     ]
-#     name = "Бронь"
-#     name_plural = "Брони"
-#     icon = "fa-solid fa-book"
+    page_size = 50
+    page_size_options = [25, 50, 100, 200]
