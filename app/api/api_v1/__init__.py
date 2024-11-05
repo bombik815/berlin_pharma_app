4dc525ea4f5f8d7000aae1793c2abf17cf1f684e
bombik815 <bombik815@gmail.com>
2024-11-05 14:44:14 +0300

    add router for release  form

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from core.config import settings
 
 from .registration_certificate.router import router as registration_certificate_router
+from .release_form.router import router as release_form_router
 
 
 router = APIRouter(
@@ -13,4 +14,8 @@ router = APIRouter(
 router.include_router(
     registration_certificate_router,
     prefix=settings.api.v1.registration_certificates,
+)
+router.include_router(
+    release_form_router,
+    prefix=settings.api.v1.release_form,
 )
